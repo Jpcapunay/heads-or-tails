514686e8dc7414c1debce2c73bf88b5f3c63430e
Jpcapunay <jeanpierrecapunay17@hotmail.com>
2023-10-17 17:10:29 -0400

    needs updating

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
-// const emojis = [🇦🇱🇦🇱🇵🇪🇵🇪🇩🇴🇩🇴]
-function flipCoin() {
-    let flipResult = Math.floor(Math.random() * 2);
-    let resultElement = document.getElementById('result');
-    if (flipResult = 0) {
 
+// document.getElementById('flipcoin').onclick = function() {
+//     const coin = document.getElementById('coin');
+//     const resultText = document.createElement('p');
+//     document.body.appendChild(resultText);
+
+  
+//     coin.style.animation = "flipCoin 1s forwards";
+
+//     coin.onanimationend = () => {
+//         let result = Math.random() < 0.5 ? 'Heads' : 'Tails';
+//         resultText.textContent = 'Result: ' + result;
+
+       
+//         coin.style.animation = '';
+//     };
+// };
+
+const card = document.getElementById("card")
+
+card.addEventListener("click", flipCard)
+
+function flipCard(){
+    card.classList.toggle("flipCard")
 }
