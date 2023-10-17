246dad9d183ef3aca3625d22b2623c8d06b07ab0
Jpcapunay <jeanpierrecapunay17@hotmail.com>
2023-10-17 10:26:56 -0400

    in process

---
+++ script.js
@@ -0,0 +1,7 @@
+// const emojis = [🇦🇱🇦🇱🇵🇪🇵🇪🇩🇴🇩🇴]
+function flipCoin() {
+    let flipResult = Math.floor(Math.random() * 2);
+    let resultElement = document.getElementById('result');
+    if (flipResult = 0) {
+
+}
